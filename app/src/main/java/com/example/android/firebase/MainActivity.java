b43111e953b5ee8ed4250766aa2046885c275984
Michael Alejandro Ura Acosta <Ingmicha@gmail.com>
2016-09-26 19:09:29 -0500

    Firebase
    Correccion de json

--- app/src/main/java/com/example/android/firebase/MainActivity.java
+++ app/src/main/java/com/example/android/firebase/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.android.firebase;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,10 +13,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
